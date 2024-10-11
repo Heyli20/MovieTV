e66f0655b7b333d6da5e9f8c949e59d8bef75f8b
Heyli20 <168403307+Heyli20@users.noreply.github.com>
2024-10-11 09:39:09 +0530

    Update Admin.java

--- src/main/webapp/adminDash/Admin.java
+++ src/main/webapp/adminDash/Admin.java
@@ -5,24 +5,12 @@ public class Admin {
 	private String email;
 	private String pass;
 	private String role;
-	static int uid=15;
-	
-	public static void userId() {
-		uid++;
-	}
 	
 	public Admin(String uname, String email, String pass, String role) {
-		super();
 		this.uname = uname;
 		this.email = email;
 		this.pass = pass;
 		this.role = "admin";
-	}
-
-	public Admin(String uname, String email) {
-		super();
-		this.uname = uname;
-		this.email = email;
 	}
 
 	public String getUname() {
@@ -31,5 +19,9 @@ public class Admin {
 
 	public String getEmail() {
 		return email;
+	}
+
+	public String getPass() {
+		return pass;
 	}
 }
