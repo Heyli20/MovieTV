f01ac0be261c68e7e1dae89390bf11309ddff113
Heyli20 <168403307+Heyli20@users.noreply.github.com>
2024-10-11 09:40:17 +0530

    Update AdminServlet.java

--- src/main/webapp/adminDash/AdminServlet.java
+++ src/main/webapp/adminDash/AdminServlet.java
@@ -1,16 +1,29 @@
 package adminDash;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 public class AdminServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		try {
+			ArrayList<Admin> adDetails = new ArrayList<>();
+			adDetails = AdminDBUtil.getAdDetails();
+			
+			request.setAttribute("adDetails","adDetails");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		RequestDispatcher dis = request.getRequestDispatcher("Dashboard/adminDash.jsp");
+		dis.forward(request, response);
 	}
 
 }
